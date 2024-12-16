f5473539a4a673350d83245c4202252f1f2091a9
Nguen <zhanna.nguen@mail.ru>
2024-12-16 21:58:33 +0300

    Занятие 13

---
+++ src/main/java/net/xonich/collections/Demo6.java
@@ -0,0 +1,23 @@
+package net.xonich.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo6 {
+
+    public static void main(String[] args) {
+
+        String[] strings = new String[10];
+        strings[0] = "qwe";
+        Object[] objects = strings;
+        objects[1] = 42;
+
+        List<String> strs = new ArrayList<>();
+        strs.add("asd");
+//        List<Object> objs = strs;
+
+        Integer i = 42;
+        Long l = 11L;
+        Number n = l;
+    }
+}
