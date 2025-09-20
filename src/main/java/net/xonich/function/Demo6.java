2f99856269c4438cb5fe246eba0dea1329ea106c
Nguen <zhanna.nguen@mail.ru>
2025-09-20 13:54:34 +0300

    Занятие 32

---
+++ src/main/java/net/xonich/function/Demo6.java
@@ -0,0 +1,50 @@
+package net.xonich.function;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Demo6 {
+
+    public static void mainCount(String[] args) {
+
+        List<String> strings = List.of("aa", "bb", "ab", "dfdf", "zhanna");
+        Stream<String> stream = strings.stream();
+//        stream.parallel();
+        System.out.println(stream.count());
+//        System.out.println(stream.count());
+    }
+
+    public static void main(String[] args) {
+
+        List<String> strings = List.of("aa", "bb", "ab", "dfdf", "zhanna");
+        long cnt = strings.stream()
+                .filter(string -> string.startsWith("a"))
+                .count();
+
+        System.out.println(cnt);
+
+        List<String> list = strings.stream()
+                .map(string -> string.substring(0, 1).toUpperCase() + string.substring(1))
+                .toList();
+
+        System.out.println(list);
+
+        strings.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+
+        boolean allMatch = strings.stream()
+                .filter(s -> s.charAt(0) != 'd' && s.charAt(0) != 'z')
+                .allMatch(s -> s.length() == 2);
+
+        System.out.println(allMatch);
+
+        Optional<String> max = strings.stream()
+                .max(Comparator.naturalOrder());
+
+        System.out.println(max);
+
+    }
+}
